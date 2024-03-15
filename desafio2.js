e3b15b6726b05613c75e5c5ed919d7f531cc6334
Ramon Coelho Melo <160602381+ramonespier@users.noreply.github.com>
2024-03-15 14:47:53 +0000

    meudeus socorro porfavor

--- desafio2.js
+++ desafio2.js
@@ -1,4 +1,32 @@
 //Crie um programa que calcule o Índice de Massa Corporal (IMC) de uma pessoa.
 
 //Orientações:
-// *Utilize o método `prompt()` do objeto `window` para realizar
+
+//Utilize o método prompt() do objeto window para realizar a leitura dos dados de entrada.
+//Utilize a fórmula IMC = peso / altura² para calcular o IMC.
+//Utilize a estrutura if-else para classificar o IMC de acordo com a tabela da OMS:
+//IMC	Indicação
+//Abaixo de 18,5	Abaixo do peso
+//18,5 - 24,9	Peso normal
+//25,0 - 29,9	Sobrepeso
+//30,0 - 34,9	Obesidade grau I
+//35,0 - 39,9	Obesidade grau II
+//Acima de 40,0	Obesidade grau III
+
+const altura = window.prompt('Informe sua altura');
+const peso = window.prompt('Informe seu peso');
+const imc = peso / (altura **2);
+
+if (imc <= 18.5) {
+    window.alert ('Abaixo do peso.')
+} else if (imc <= 24.9) {
+    window.alert ('Peso normal.')
+} else if (imc <= 29.9) {
+    window.alert ('Sobrepeso.')
+} else if (imc <= 34.9) {
+    window.alert ('Obesidade grau I.')
+} else if (imc <= 39.9) {
+    window.alert ('Obesidade grau II.')
+} if (imc <= 40.0) {
+    window.alert ('Obesidade grau III.')
+};
---
+++ operadores-logicos.js
@@ -0,0 +1,11 @@
+const a = true, b = true;
+
+if (a || b) {
+    console.log('VERDADEIRO')
+} else {
+    console.log('FALSO')
+}
+
+//&& -> E
+// || -> OU
+// ! -> NÃO
