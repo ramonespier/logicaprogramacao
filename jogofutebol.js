7f7f2bebf6b28c51497893231addb3663687f776
ramonespier <ramon.coelhomelo@outlook.com>
2024-04-02 11:42:53 -0300

    Resolvendo mais exercícios

--- jogofutebol.js
+++ jogofutebol.js
@@ -1,20 +1,20 @@
-function placar (time1, time2,) {
+function placar (time1, time2, gol1, gol2) {
 
     let mensagem;
 
     if (time1 > time2) {
-        mensagem = 'O Corinthians está ganhando do Palmeiras'
+        mensagem = `O Corinthians está ganhando do Palmeiras de ${gol1} x ${gol2}.`
 
     } else if (time2 > time1){
-        mensagem = 'O Palmeiras está ganhando do Corinthians'
+        mensagem = `O Palmeiras está ganhando do Corinthians de ${gol2} x ${gol1}.`
 
     } else {
-        mensagem = 'O jogo está empatado'
+        mensagem = `O jogo está empatado, ${gol1} x ${gol2}.`
     }
 
 return {
-
-    mensagem: mensagem
-    
+    mensagem: mensagem,
+    gol1: gol1,
+    gol2: gol2
     }
 }
--- triangulo.js
+++ triangulo.js
@@ -5,8 +5,8 @@ function tipoTriangulo (ladoA, ladoB, ladoC) {
     if ((ladoA === ladoB) && (ladoB === ladoC)) {
         mensagem = 'Como todos os lados são iguais, o triângulo é equilátero.'
 
-    } else if ((ladoA === ladoB) || (ladoB === ladoC)) {
-        mensagem = 'Se dois lados são iguais e apenas uma lado é diferente o triângulo é isóceles'
+    } else if ((ladoA === ladoB) || (ladoB === ladoC) || (ladoA === ladoC)) {
+        mensagem = 'Se dois lados são iguais e apenas uma lado é diferente o triângulo é isósceles'
 
     } else {
         mensagem = 'Se todos lados são diferentes, o triângulo é escaleno'
