fba050f17a17b4a26640a4dd31c6915949cfbc25
ramonespier <ramon.coelhomelo@outlook.com>
2024-04-05 11:24:10 -0300

    Estilo

--- triangulo.js
+++ triangulo.js
@@ -3,13 +3,13 @@ function tipoTriangulo (ladoA, ladoB, ladoC) {
     let mensagem;
 
     if ((ladoA === ladoB) && (ladoB === ladoC)) {
-        mensagem = 'Como todos os lados são iguais, o triângulo é equilátero.'
+        mensagem = 'Equilátero.'
 
     } else if ((ladoA === ladoB) || (ladoB === ladoC) || (ladoA === ladoC)) {
-        mensagem = 'Se dois lados são iguais e apenas uma lado é diferente o triângulo é isósceles'
+        mensagem = 'Isósceles.'
 
     } else {
-        mensagem = 'Se todos lados são diferentes, o triângulo é escaleno'
+        mensagem = 'Escaleno.'
     }
     
     return {
