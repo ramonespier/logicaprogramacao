b3ba5145fb4db80f6547fcd78ebd3cdd781af37b
ramonespier <ramon.coelhomelo@outlook.com>
2024-04-13 11:22:55 -0300

    test: mudando o corpo do codigo com diferentes funcões

--- exercises/desconto.js
+++ exercises/desconto.js
@@ -1,23 +1,45 @@
-// Uma loja fornece 10% de desconto para funcionários e 5% de desconto para clientes VIPs.
-// Faça um programa que calcule o valor total a ser pago por uma pessoa. O programa deverá ler o valor total da compra efetuada e um código que identifique se o comprador é um cliente comum [1], funcionário [2] ou VIP [3].
-// A saída do programa deve ser o valor total da compra após o processamento e a categoria do desconto aplicado, se houver.
-// Dica: para calcular um percentual de um número, faça a operação desconto = valor * percentualDesconto / 100
+function compra() {
 
-function desconto (cliente) {
-
-    let valor = 100.00
- 
-    if (cliente === `Comum`) {
-        mensagem = `O valor a ser pago não terá desconto, pagará ${valor}`;
-
-    } else if (cliente === `Funcionário`) {
-        mensagem = `O cliente terá 10% de desconto, irá pagar ${valor - (valor *0.1)}`
-
+    let codigo = parseInt(document.getElementById("codigo").value);
+    let valor = parseFloat(document.getElementById("valor").value);
+    let resultado = valor;
+    
+    if (valor > 0) {
+        if (codigo === 1) {
+            resultado = valor;
+    
+            document.querySelector('p.resultado').innerHTML = `Valor a pagar R$` + resultado.toFixed(2)
+            document.querySelector('p.erro').innerHTML = "";
+    
+        } else if (codigo === 2) {
+        resultado -= (valor * 0.1);
+    
+        document.querySelector('p.resultado').innerHTML = 'Valor a pagar R$' + resultado.toFixed(2);
+        document.querySelector('p.erro').innerHTML = "";
+    
+        } else if (codigo === 3) {
+        resultado -= (valor * 0.05);
+    
+        document.querySelector("p.resultado").innerHTML = 'Valor a pagar R$' + resultado.toFixed(2);
+        document.querySelector('p.erro').innerHTML = "";
+    
+        } else {
+    
+        document.querySelector("p.erro").innerHTML = "Código inválido!";
+        document.querySelector("p.resultado").innerHTML = "";
+        }
+    
     } else {
-        mensagem = `O cliente terá 5% de desconto, irá pagar ${valor - (valor *0.05)}`
+    
+    document.querySelector("p.erro").innerHTML = "Digite um valor válido!";
+    document.querySelector("p.resultado").innerHTML = "";
+    
     }
-
-    return{
-        mensagem: mensagem
-    }
-}
+};
+    
+    function limpar() {
+    document.getElementById('valor').value = "";
+    document.getElementById('codigo').value = "";
+    document.querySelector('p.resultado').innerHTML = "";
+    document.querySelector('p.erro').innerHTML = "";
+};
