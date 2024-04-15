6fa007781e492cf7b2a8490a24aac9ac2fde3ca2
ramonespier <ramon.coelhomelo@outlook.com>
2024-04-15 08:55:47 -0300

    chore: predefinindo os botões de trocar o tema que estavam no html

--- javascript/tema.js
+++ javascript/tema.js
@@ -9,3 +9,18 @@ document.querySelector('#tema-claro').addEventListener('click', function trocaTe
     if (document.documentElement.getAttribute('data-tema') === 'dark') {
         document.documentElement.setAttribute('data-tema', 'light'); }
     });
+
+    document.querySelector('#tema-claro').addEventListener('click', () => {
+
+        document.querySelector('#tema-claro').classList.add('oculto');
+        document.querySelector('#tema-escuro').classList.remove('oculto');
+        
+
+    });
+
+    document.querySelector('#tema-escuro').addEventListener('click', () => {
+
+        document.querySelector('#tema-claro').classList.remove('oculto');
+        document.querySelector('#tema-escuro').classList.add('oculto');
+        
+    });
