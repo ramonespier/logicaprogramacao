1aed70973aa81851b4e8734460b2c6513bfb3229
ramonespier <ramon.coelhomelo@outlook.com>
2024-04-15 22:13:24 -0300

    fix: corrigindo undefined, porem agora tem bugs

--- exercises/posto-de-gasolina.js
+++ exercises/posto-de-gasolina.js
@@ -1,41 +1,60 @@
 function postoGasolina (totalLitros, codigo) {
-    let alcool = 2.89;
-    let gasolina = 5.19;
-
-    totalLitros = document.querySelector('#litros');
-    codigo = document.querySelector('#codigo');
+    const alcool = 2.89;
+    const gasolina = 5.19;
 
     if (document.querySelector(codigo === 'A')) {
 
-        if (totalLitros < 21) {
-            valor = (alcool * totalLitros) - (totalLitros * 0.03);
+        if (totalLitros <= 20) {
+            return (alcool * totalLitros) - (totalLitros * 0.03);
     
+        } else if (totalLitros > 20) {
+            return (alcool * totalLitros) - (totalLitros * 0.05);
+
         } else {
-            valor = (alcool * totalLitros) - (totalLitros * 0.05);
+            return "Digite um carácter válido."
+
         };
         
+    // } else if (document.querySelector(codigo === 'G')) {
     } else {
 
-        if (totalLitros < 21) {
-            valor = (gasolina * totalLitros) - (totalLitros * 0.04);
+        if (totalLitros <= 20) {
+            return (gasolina * totalLitros) - (totalLitros * 0.04);
     
-        } else {
-            valor = (gasolina * totalLitros) - (totalLitros * 0.06);
-        };        
-    };
+        } else if (totalLitros > 20) {
+            return (gasolina * totalLitros) - (totalLitros * 0.06);
 
-    return {
-    valor: valor
+        } else {
+            return "Digite um carácter válido."
+
+        };  
+
     };
 };
+    // } else {
+
+    //     if (codigo !== 'A' || 'G') {
+    //         return 'Selecione "A" OU "G"'
+
+    //     } else if (totalLitros < 0) {
+    //         return 'Digite um número positivo.'
+
+    //     } else if (totalLitros > 120) {
+    //         return 'O tanque vai transbordar.'
+
+    //     } else {
+    //         return 'Recarregue a página'
+            
+    //     };
+    // };
 
     document.querySelector('#calc-gasolina').addEventListener('click', () => {
-        const totalLitros = document.querySelector('#litros');
-        const codigo = document.querySelector('#codigo');
+        const totalLitros = parseFloat(document.querySelector('#litros').value);
+        const codigo = document.querySelector('#codigo').value;
 
         const desconto = postoGasolina (totalLitros, codigo);
 
-        document.querySelector('#resultado-gasolina').innerHTML = `Valor a ser pago é de R$${desconto.valor}`
+        document.querySelector('#resultado-gasolina').innerHTML = `${desconto.toFixed(2)}`
     });
 
     document.querySelector('#limpa-gasolina').addEventListener('click', function(){
