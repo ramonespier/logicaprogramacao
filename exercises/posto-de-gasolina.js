ded598e099e011b05633b59ba1568cf85284d872
ramonespier <ramon.coelhomelo@outlook.com>
2024-04-16 09:33:21 -0300

    fix: correção da conta do alcool

--- exercises/posto-de-gasolina.js
+++ exercises/posto-de-gasolina.js
@@ -1,60 +1,36 @@
-function postoGasolina (totalLitros, codigo) {
+function postoGasolina (litros, codigo) {
     const alcool = 2.89;
     const gasolina = 5.19;
+    let valor;
 
-    if (document.querySelector(codigo === 'A')) {
+    if (codigo === 'A' && litros <= 20) {
+        valor = (alcool * litros) - (litros * 0.03);
 
-        if (totalLitros <= 20) {
-            return (alcool * totalLitros) - (totalLitros * 0.03);
+    } else if (codigo === 'A' && litros > 20) {
+        valor = (alcool * litros) - (litros * 0.05);
+
+    } else if (codigo === 'G' && litros <= 20) {
+        valor = (gasolina * litros) - (litros * 0.04);
+
+    } else if (codigo === 'G' && litros > 20) {
+        valor = (gasolina * litros) - (litros * 0.06);
     
-        } else if (totalLitros > 20) {
-            return (alcool * totalLitros) - (totalLitros * 0.05);
-
-        } else {
-            return "Digite um carácter válido."
-
-        };
-        
-    // } else if (document.querySelector(codigo === 'G')) {
     } else {
+        'Digite um código ou valor válido.'
+    }
 
-        if (totalLitros <= 20) {
-            return (gasolina * totalLitros) - (totalLitros * 0.04);
-    
-        } else if (totalLitros > 20) {
-            return (gasolina * totalLitros) - (totalLitros * 0.06);
-
-        } else {
-            return "Digite um carácter válido."
-
-        };  
-
-    };
+    return valor;
 };
-    // } else {
 
-    //     if (codigo !== 'A' || 'G') {
-    //         return 'Selecione "A" OU "G"'
 
-    //     } else if (totalLitros < 0) {
-    //         return 'Digite um número positivo.'
-
-    //     } else if (totalLitros > 120) {
-    //         return 'O tanque vai transbordar.'
-
-    //     } else {
-    //         return 'Recarregue a página'
-            
-    //     };
-    // };
 
     document.querySelector('#calc-gasolina').addEventListener('click', () => {
-        const totalLitros = parseFloat(document.querySelector('#litros').value);
+        const litros = parseFloat(document.querySelector('#litros').value);
         const codigo = document.querySelector('#codigo').value;
 
-        const desconto = postoGasolina (totalLitros, codigo);
+        const desconto = postoGasolina (litros, codigo);
 
-        document.querySelector('#resultado-gasolina').innerHTML = `${desconto.toFixed(2)}`
+        document.querySelector('#resultado-gasolina').innerHTML = 'Valor a ser pago: R$' + desconto.toFixed(2);
     });
 
     document.querySelector('#limpa-gasolina').addEventListener('click', function(){
