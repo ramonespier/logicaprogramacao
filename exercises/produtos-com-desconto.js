a241044c56926b3c99a1a7ff17a49d9cc2998121
ramonespier <ramon.coelhomelo@outlook.com>
2024-04-22 09:01:16 -0300

    feat: começo do script

---
+++ exercises/produtos-com-desconto.js
@@ -0,0 +1,18 @@
+function produtosComDesconto () {
+    let notebook = 4.000;
+    let gabinete = 350;
+    let processador = 650;
+
+    const produto = document.querySelector('#produto') 
+    const quantidade = document.querySelector('#quantidade').value;
+
+    if(produto === '1') {
+        
+        if (quantidade  ) {
+
+        }
+
+    }
+
+
+}
