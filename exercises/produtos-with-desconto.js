07aba5661fbdaec7744f008929de042c591b6a46
ramonespier <ramon.coelhomelo@outlook.com>
2024-04-25 17:46:28 -0300

    refactor: continua não funcionando

--- exercises/produtos-with-desconto.js
+++ exercises/produtos-with-desconto.js
@@ -4,75 +4,63 @@ const quantidade = document.querySelector('#quantidade').value;
 
 function produtosComDesconto (produto, quantidade) {
 
-    let pro1;
-    let pro2;
-    let pro3;
+    const pro1 = quantidade * 4000;
+    const pro2 = quantidade * 350;
+    const pro3 = quantidade * 650;
     let valor;
 
 
-    if(produto === '1') {
+    if(produto === '1' && quantidade > '0' && quantidade <= '3') {
+        valor = pro1;        
         
-        if (quantidade <= '2') {
-            valor = 4000;
+    } else if (produto === '1' && quantidade <= '5') {
+        valor = pro1 - (pro1 * 0.02)
             
-            } else if (quantidade <= '5') {
-                valor = pro1 = 4000 - (4000 * 0.02);
+    } else if (produto === '1' && quantidade <= '10') {
+         valor = pro1 - (pro1 * 0.03);
                 
-            } else if (quantidade <= '10') {
-                valor = pro1 = 4000 - (4000 * 0.03);
+    } else if (produto === '1' && quantidade > '10') {
+        valor = pro1 - (pro1 * 0.05);  
                 
-            } else  if (quantidade >= '11') {
-                valor = pro1 = 4000 - (4000 * 0.05);  
-
-            } else {
-                valor = 'ERRO: Insira um carácter válido.'
-            }
-
-        } else if (produto === '2') {
-        
-            if (quantidade <= '2') {
-                valor = produto = pro2;
-                
-                } else if (quantidade <= '5') {
-                    valor = pro2 - (pro2 * 0.02);
+    } else if (produto === '2' && quantidade <= '2') {
+        valor = 350;
                     
-                } else if (quantidade <= '10') {
-                    valor = pro2 - (pro2 * 0.03);
+    } else if (produto === '2' && quantidade <= '5') {
+        valor = pro2 - (pro2 * 0.02);
                     
-                } else  if (quantidade >= '11') {
-                    valor = pro2 - (pro2 * 0.05);  
+    } else if (produto === '2' && quantidade <= '10') {
+        valor = pro2 - (pro2 * 0.03);
+                    
+    } else  if (produto === '2' && quantidade >= '11') {
+        valor = pro2 - (pro2 * 0.05);                                                  
+
+    } else if (produto === '3' && quantidade <= '2') {
+        valor = 650;
+                
+    } else if (produto === '3' && quantidade <= '5') {
+        valor = pro3 - (pro3 * 0.02);
+                    
+    } else if (produto === '3' && quantidade <= '10') {
+        valor = pro3 - (pro3 * 0.03);
+                    
+    } else  if (produto === '3' && quantidade >= '11') {
+        valor = pro3 - (pro3 * 0.05);  
     
-                } else {
-                    valor = 'ERRO: Insira um carácter válido.'
+    } else {
+        valor = 'ERRO: Insira um carácter válido.'
 
-                }
+    }
 
-        } else {
-            if (quantidade <= '2') {
-                valor = produto = pro3;
-                
-                } else if (quantidade <= '5') {
-                    valor = pro3 - (pro3 * 0.02);
-                    
-                } else if (quantidade <= '10') {
-                    valor = pro3 - (pro3 * 0.03);
-                    
-                } else  if (quantidade >= '11') {
-                    valor = pro3 - (pro3 * 0.05);  
-    
-                } else {
-                    valor = 'ERRO: Insira um carácter válido.'
-
-                }
-
-        }
-    
-    return valor;
+    return {
+        valor: valor,
+        produto: produto,
+        quantidade: quantidade
+    }  
+} 
     // console.log(produtosComDesconto(produto, quantidade));
-};
 
     document.querySelector('#calc-produto').addEventListener('click', () => {
-        const produto = document.querySelector('#produto').value 
+        const produto = document.querySelector('#produto').value; 
         const quantidade = document.querySelector('#quantidade').value;
         
         const produtoDesconto = produtosComDesconto (produto, quantidade)
